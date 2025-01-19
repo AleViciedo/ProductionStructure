4662635c72cefbdc9b0fd7bafe4dded15d327189
AleViciedo <aleviciedo@gmail.com>
2025-01-18 23:16:36 -0500

    Created Base for Repositories

---
+++ DataAccess/Repositories/Common/RepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionStructure.DataAccess.Repositories.Common
+{
+    public abstract class RepositoryBase
+    {
+        protected ApplicationContext _context;
+        protected RepositoryBase(ApplicationContext context)
+        {
+            _context = context;
+        }
+    }
+}
