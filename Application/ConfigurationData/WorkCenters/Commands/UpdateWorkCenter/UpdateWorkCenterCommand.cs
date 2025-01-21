75d8b274a0281ec640bd252ff5de5155141215ec
AleViciedo <aleviciedo@gmail.com>
2025-01-20 22:59:36 -0500

    Files for Commands and Queries created

---
+++ Application/ConfigurationData/WorkCenters/Commands/UpdateWorkCenter/UpdateWorkCenterCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionStructure.Application.HistoricalData.WorkSessions.Commands.UpdateWorkSession
+{
+    internal class UpdateWorkCenterCommand
+    {
+    }
+}
