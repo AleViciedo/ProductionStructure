855ead06fe9ddf3b5cac2d42914edbc250a72310
AleViciedo <aleviciedo@gmail.com>
2025-01-20 20:25:19 -0500

    Ready for implementing CQRS

---
+++ Application/Abstract/IQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace ProductionStructure.Application.Abstract
+{
+    public interface IQuery<TResponse> : IRequest<TResponse>
+    {
+    }
+}
